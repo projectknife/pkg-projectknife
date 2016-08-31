e59b6de9378dedef06a994e6c108f28fa3f8922d
eaxs <tkuhn@live.com>
2016-08-31 15:10:00 +0200

    Added: Create component menu items upon install

--- source/components/com_pkmilestones/setup.php
+++ source/components/com_pkmilestones/setup.php
@@ -39,17 +39,65 @@ class com_pkmilestonesInstallerScript
             $db->setQuery($query);
             $exists = (int) $db->loadResult();
 
-            if ($exists) {
-                return true;
+            // Register the extension
+            if (!$exists) {
+                $ext             = new stdClass();
+                $ext->id         = $id;
+                $ext->admin_view = 'milestones';
+                $ext->ordering   = 3;
+
+                $db->insertObject('#__pk_extensions', $ext);
             }
 
-            // Register the extension
-            $ext             = new stdClass();
-            $ext->id         = $id;
-            $ext->admin_view = 'milestones';
-            $ext->ordering   = 3;
 
-            $db->insertObject('#__pk_extensions', $ext);
+            // Check if Projectknife menu exists
+            $query->clear()
+                  ->select('id')
+                  ->from('#__menu_types')
+                  ->where('menutype = ' . $db->quote('projectknife'));
+
+            $db->setQuery($query);
+            $menu_id = (int) $db->loadResult();
+
+            // Create nav menu item
+            if ($menu_id) {
+                $data = array(
+                    'title'        => 'Milestones',
+                    'alias'        => 'milestones',
+                    'link'         => 'index.php?option=com_pkmilestones&view=list',
+                    'component_id' => $id,
+                    'menutype'     => 'projectknife',
+                    'parent_id'    => 1,
+                    'level'        => 1,
+                    'published'    => 1,
+                    'type'         => 'component',
+                    'language'     => '*',
+                    'access'       => 1,
+                    'params'       => '{}',
+                    'ordering'     => 0,
+                    'id'           => null
+                );
+
+                $menu = JTable::getInstance('menu');
+                $menu->setLocation(1, 'last-child');
+
+                if ($menu->bind($data) && $menu->check() && $menu->store()) {
+                    $query->clear();
+                    $query->update('#__menu')
+                          ->set('parent_id = 1')
+                          ->set('level = 1')
+                          ->where('id = ' . (int) $menu->id);
+
+                    $db->setQuery($query);
+                    $db->execute();
+
+                    $menu->parent_id = 1;
+                    $menu->level     = 1;
+
+                    $menu->setLocation(1, 'last-child');
+                    $menu->rebuildPath($menu->id);
+                }
+            }
         }
 
         return true;
--- source/components/com_pkprojects/setup.php
+++ source/components/com_pkprojects/setup.php
@@ -39,17 +39,65 @@ class com_pkprojectsInstallerScript
             $db->setQuery($query);
             $exists = (int) $db->loadResult();
 
-            if ($exists) {
-                return true;
+            // Register the extension
+            if (!$exists) {
+                $ext             = new stdClass();
+                $ext->id         = $id;
+                $ext->admin_view = 'projects';
+                $ext->ordering   = 2;
+
+                $db->insertObject('#__pk_extensions', $ext);
             }
 
-            // Register the extension
-            $ext             = new stdClass();
-            $ext->id         = $id;
-            $ext->admin_view = 'projects';
-            $ext->ordering   = 2;
 
-            $db->insertObject('#__pk_extensions', $ext);
+            // Check if Projectknife menu exists
+            $query->clear()
+                  ->select('id')
+                  ->from('#__menu_types')
+                  ->where('menutype = ' . $db->quote('projectknife'));
+
+            $db->setQuery($query);
+            $menu_id = (int) $db->loadResult();
+
+            // Create nav menu item
+            if ($menu_id) {
+                $data = array(
+                    'title'        => 'Projects',
+                    'alias'        => 'projects',
+                    'link'         => 'index.php?option=com_pkprojects&view=list',
+                    'component_id' => $id,
+                    'menutype'     => 'projectknife',
+                    'parent_id'    => 1,
+                    'level'        => 1,
+                    'published'    => 1,
+                    'type'         => 'component',
+                    'language'     => '*',
+                    'access'       => 1,
+                    'params'       => '{}',
+                    'ordering'     => 0,
+                    'id'           => null
+                );
+
+                $menu = JTable::getInstance('menu');
+                $menu->setLocation(1, 'last-child');
+
+                if ($menu->bind($data) && $menu->check() && $menu->store()) {
+                    $query->clear();
+                    $query->update('#__menu')
+                          ->set('parent_id = 1')
+                          ->set('level = 1')
+                          ->where('id = ' . (int) $menu->id);
+
+                    $db->setQuery($query);
+                    $db->execute();
+
+                    $menu->parent_id = 1;
+                    $menu->level     = 1;
+
+                    $menu->setLocation(1, 'last-child');
+                    $menu->rebuildPath($menu->id);
+                }
+            }
         }
 
         return true;
